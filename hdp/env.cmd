93b13680f6ad6a726a2448bbcc5af1c81b1e29da
Mike <MikeXL@users.noreply.github.com>
2015-04-23 12:38:39 -0700

    Create env.cmd

---
+++ hdp/env.cmd
@@ -0,0 +1,20 @@
+#Just an example, you can create your own
+
+SET HADOOP_HOME=E:\hdp-2.2.4.2\hadoop-2.6.0.2.2.4.2-000
+SET HIVE_HOME=E:\hdp-2.2.4.2\hive-0.14.0.2.2.4.2-0002
+SET HADOOP_PREFIX=E:\hdp-2.2.4.2\hadoop-2.6.0.2.2.4.2-0002
+SET HADOOP_CONF_DIR=%HADOOP_HOME%\etc\hadoo
+SET YARN_CONF_DIR=%HADOOP_CONF_DIR%
+SET PATH=%PATH%;%HADOOP_HOME%\bin;%HIVE_HOME%/bin
+
+# to start dfs 
+# start-dfs
+
+# start yarn
+# start-yarn
+
+# start hive server 2
+# %hive_home%\bin\hive --service hiveserver2
+
+#start hive client
+# hive
